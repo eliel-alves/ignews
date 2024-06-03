81fdfc662d5c1bf7e6b69ff31ea041e102be80c9
Eliel Alves <elielalves.dev@gmail.com>
2024-06-03 17:53:04 -0300

    feat: add app header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from 'next/app';
 
+import { Header } from '@/components/Header';
+
 import '../styles/global.scss'
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   )
 }
