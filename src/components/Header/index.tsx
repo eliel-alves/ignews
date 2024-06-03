12d78db4eff5b75082c028eaf250c7db3c8b976c
Eliel Alves <elielalves.dev@gmail.com>
2024-06-03 20:19:50 -0300

    feat: ssg config to homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss'
@@ -8,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" width={109} height={31} alt="ig.news" />
+        <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
           <a className={styles.active} href="">Home</a>
