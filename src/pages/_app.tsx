ebc9de021834373be221b7a735933b5dce9bb76a
Eliel Alves <elielalves.dev@gmail.com>
2024-06-03 17:29:11 -0300

    feat: add global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
+
+import '../styles/global.scss'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
